3377e77ab377ae8603d8417529f2b5d8ed617fc2
theras <theras@itba.edu.ar>
2025-09-21 17:29:08 -0300

    separar auth en capas

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { supabase } from '../config/supabase';
+import { logInWithPasswordService, signInWithPasswordService } from '../services/authService';
 
 export const signInWithPassword = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -7,28 +8,7 @@ export const signInWithPassword = async (req: Request, res: Response, next: Next
 
     console.log("Intento de registro:", { email, name }); // Para debug
 
-    if (!email || !password || !name) {
-      return res.status(400).json({
-        error: "Email, contraseña y nombre son requeridos"
-      });
-    }
-
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        data: {
-          full_name: name,
-        }
-      }
-    });
-
-    if (error) {
-      console.log("Error de Supabase en registro:", error.message);
-      return res.status(400).json({
-        error: error.message
-      });
-    }
+    const data = await signInWithPasswordService(email, password, name);
 
     console.log("Registro exitoso para:", email);
     res.status(201).json({
@@ -49,22 +29,7 @@ export const logInWithPassword = async (req: Request, res: Response, next: NextF
   try {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({
-        error: "Email y contraseña son requeridos"
-      });
-    }
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      return res.status(401).json({
-        error: error.message
-      });
-    }
+    const data = await logInWithPasswordService(email, password);
 
     res.status(200).json({
       success: true,
---
+++ server/src/services/authService.ts
@@ -0,0 +1,60 @@
+import { supabase } from "../config/supabase";
+import { AppError } from "../errors/AppError";
+
+export async function signInWithPasswordService(email: string, password: string, name: string) {
+    if (!email || !password || !name) {
+        throw new AppError("Email, contraseña y nombre son requeridos.", 400);
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: {
+                full_name: name,
+            }
+        }
+    });
+
+    if (error) {
+        console.log("Error de Supabase en registro:", error.message);
+        throw new AppError(error.message, 400);
+    }
+
+    return data;
+}
+
+export async function logInWithPasswordService(email: string, password: string) {
+    if (!email || !password) {
+        throw new AppError("Email, contraseña y nombre son requeridos.", 400);
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+
+    if (error) {
+        throw new AppError(error.message, 401);
+    }
+
+    return data;
+}
+
+// export async function getProfileByPhone(phone: string) {
+//     if (!phone) {
+//         throw new AppError("", 400);
+//     }
+
+//     const { data, error } = await supabase
+//         .from("profiles")
+//         .select("*")
+//         .eq("phone", phone)
+//         .single();
+
+//     if (error) {
+//         throw new AppError(error.message, 500);
+//     }
+
+//     return data;
+// }
