4ffb11c2c7504fad6ab9ddde88da55a3dd742bf6
theras <theras@itba.edu.ar>
2025-10-03 16:51:33 -0300

    agregar creación y obtencion de perfiles

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,7 +1,7 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { supabase } from '../config/supabase';
+import { NextFunction, Request, Response } from 'express';
 import { logInWithPasswordService, signInWithPasswordService } from '../services/authService';
 import { AppError } from '../errors/AppError';
+import { createProfile, getProfileByPhone } from '../services/profileService';
 
 export const signInWithPassword = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -9,13 +9,19 @@ export const signInWithPassword = async (req: Request, res: Response, next: Next
 
     console.log("Intento de registro:", { email, name }); // Para debug
 
-    const data = await signInWithPasswordService(email, password, name);
+    const { user } = await signInWithPasswordService(email, password, name);
+
+    if (!user) {
+      throw new AppError("No se pudo crear el usuario en la tabla auth", 500);
+    }
+
+    await createProfile(user.id, name);
 
     console.log("Registro exitoso para:", email);
     res.status(201).json({
       success: true,
       message: "Usuario registrado exitosamente",
-      user: data.user,
+      user: user,
     });
 
   } catch (error: AppError | any) {
@@ -45,5 +51,25 @@ export const logInWithPassword = async (req: Request, res: Response, next: NextF
     res.status(error.statusCode).json({
       error: error.message || "Error interno del servidor"
     });
+  }
+};
+
+
+export const findProfileByPhone = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { whatsapp_jid } = req.query;
+
+    if (!whatsapp_jid || typeof whatsapp_jid !== 'string') {
+      throw new AppError("El parámetro whatsapp_jid es requerido y debe ser una cadena.", 400);
+    }
+
+    const profile = await getProfileByPhone(whatsapp_jid);
+
+    res.status(200).json(profile);
+
+  } catch (error: AppError | any) {
+    console.error("Error en login:", error);
+
+    res.status(error.statusCode).json({ message: error.message });
   }
 };
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { logInWithPassword, signInWithPassword } from '../controllers/authControllers';
+import { findProfileByPhone, logInWithPassword, signInWithPassword } from '../controllers/authControllers';
 
 const authRouter = express.Router();
 
 authRouter.post("/register", signInWithPassword);
 
 authRouter.post("/login", logInWithPassword);
+
+authRouter.get("/profile", findProfileByPhone);
 
 export default authRouter;
