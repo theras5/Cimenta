bc210111ff7bd273ba6ab7f6e7b46495e5b5b5fd
theras <theras@itba.edu.ar>
2025-10-03 16:52:22 -0300

    agregar tipos de auth

--- packages/dtos/src/index.ts
+++ packages/dtos/src/index.ts
@@ -2,4 +2,5 @@ export * from './types/task.dto';
 export * from './types/site.dto';
 export * from './types/change.dto';
 export * from './types/update.dto';
+export * from './types/auth.dto';
 export * from './types/purchase.dto';
---
+++ packages/dtos/src/types/auth.dto.ts
@@ -0,0 +1,6 @@
+export interface Profile {
+    id: string;
+    name: string;
+    whatsapp_jid: string;
+    avatar_url?: string;
+}
