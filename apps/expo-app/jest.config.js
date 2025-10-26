8287a2e44cd0b828964af7465b06fcae5620616a
dalilaorbaj <dali.orbaj24@gmail.com>
2025-10-25 21:11:01 -0300

    no jest

--- apps/expo-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'jest-expo',
-  setupFiles: ['<rootDir>/jest.globals.js'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-};
--- apps/expo-app/jest.globals.js
+++
@@ -1,8 +0,0 @@
-// Minimal globals for jest-expo/react-native setup
-if (typeof global.window === 'undefined') {
-  global.window = {};
-}
-if (typeof global.navigator === 'undefined') {
-  global.navigator = {};
-}
-
